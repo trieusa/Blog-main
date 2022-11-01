82491fab3011cf3b7e84204b9c4bc5c5e8c4c5b6
huytran1706 <huytran.17062001@gmail.com>
2022-11-01 09:54:26 +0700

    main

--- BlogApi/Repositories/UserRepository.cs
+++ BlogApi/Repositories/UserRepository.cs
@@ -4,27 +4,90 @@ using BlogApi.Models;
 
 namespace BlogApi.Repositories
 {
-    public class UserRepository
+  public class UserRepository
     {
-        private readonly AppDbContext _appDbContext;
-        public UserRepository(AppDbContext appDbContext)
+        private readonly AppDBContext _context;
+        public UserRepository(AppDBContext context)
         {
-            _appDbContext = appDbContext;
+            _context = context;
         }
         public UserDto InsertUser(User user)
         {
-            _appDbContext.Users.Add(user);
-            _appDbContext.SaveChanges();
-            var results = new UserDto()
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            var result = new UserDto()
             {
                 DisplayName = user.DisplayName,
-                ID = user.ID,
                 Email = user.Email,
                 Phone = user.Phone,
                 Address = user.Address,
-                DateOfBirth = user.DateOfBirth,
+                DateOfBirth = user.DateOfBirth
             };
-            return results;
+            return result;
+        }
+        public async Task<List<UserDto>> GetListUser()
+        {
+            try
+            {
+                var result = await _context.Users.AsNoTracking().Select(user => new UserDto()
+                {
+                    DisplayName = user.DisplayName,
+                    Email = user.Email,
+                    ID = user.ID,
+                    Phone = user.Phone,
+                    Address = user.Address,
+                    DateOfBirth = user.DateOfBirth,
+                }).ToListAsync();
+                return result;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteUser(Guid Id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.ID == Id);
+
+            if (user == null)
+            {
+                return false;
+            };
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<UserDto> EditUser(Guid Id, User userNew)
+        {
+
+            var userExist = await _context.Users.FirstOrDefaultAsync(user => user.ID == Id);
+
+            if (userExist == null)
+            {
+                return null;
+            };
+            userExist.DisplayName = userNew.DisplayName;
+            userExist.Email = userNew.Email;
+            userExist.Phone = userNew.Phone;
+            userExist.Address = userNew.Address;
+            userExist.DateOfBirth = userNew.DateOfBirth;
+
+            await _context.SaveChangesAsync();
+
+            return new UserDto()
+            {
+                DisplayName = userExist.DisplayName,
+                Email = userExist.Email,
+                ID = userExist.ID,
+                Phone = userExist.Phone,
+                Address = userExist.Address,
+                DateOfBirth = userExist.DateOfBirth,
+            };
         }
     }
 }
